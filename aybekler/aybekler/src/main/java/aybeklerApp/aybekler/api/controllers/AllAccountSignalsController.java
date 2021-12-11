a760d999c509fcf1492cfb54b23a1a63403a9219
ByrMucahit <m.mucahitbayar@gmail.com>
2021-12-11 14:52:42 +0300

    All Accounts Signal
    All Account Signal in API has been built

---
+++ aybekler/aybekler/src/main/java/aybeklerApp/aybekler/api/controllers/AllAccountSignalsController.java
@@ -0,0 +1,26 @@
+package aybeklerApp.aybekler.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import aybeklerApp.aybekler.entities.concretes.AllAccountSignals;
+
+@RestController
+@RequestMapping("/api/allaccountsignals")
+public class AllAccountSignalsController {
+	
+	
+	
+	@Autowired
+	public AllAccountSignalsController() {
+		super();
+		
+	}
+	
+	@GetMapping("/getall")
+	public void getAll() {
+		System.out.println("It's working");
+	}
+}
