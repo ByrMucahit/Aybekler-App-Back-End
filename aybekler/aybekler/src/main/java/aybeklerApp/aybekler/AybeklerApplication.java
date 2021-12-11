b046ce5f140b37d6d111d62e0f4bd59f15dce929
ByrMucahit <m.mucahitbayar@gmail.com>
2021-12-11 14:52:56 +0300

    Main
    Main has been cleared.

--- aybekler/aybekler/src/main/java/aybeklerApp/aybekler/AybeklerApplication.java
+++ aybekler/aybekler/src/main/java/aybeklerApp/aybekler/AybeklerApplication.java
@@ -9,26 +9,20 @@ import aybeklerApp.aybekler.entities.concretes.HomeTable;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class AybeklerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AybeklerApplication.class, args);
-		/* Home Table Test */
-		
-		
-		
-		
-	}
-
-	                               
+		/* Home Table Test */	
+	}      
 	    @Bean
 	    public Docket api() { 
 	        return new Docket(DocumentationType.SWAGGER_2)  
 	          .select()                                  
 	          .apis(RequestHandlerSelectors.basePackage("aybeklerApp.aybekler"))                                      
-	          .build();                                           
-	    
+	          .build();                                              
 	}
 }
