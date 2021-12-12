b398c67389ec6cde26ef5db06d7fcc0d9f5bbb2f
ByrMucahit <m.mucahitbayar@gmail.com>
2021-12-12 17:21:29 +0300

    All Account Signals
    API has been built

--- aybekler/aybekler/src/main/java/aybeklerApp/aybekler/api/controllers/AllAccountSignalsController.java
+++ aybekler/aybekler/src/main/java/aybeklerApp/aybekler/api/controllers/AllAccountSignalsController.java
@@ -1,24 +1,31 @@
 package aybeklerApp.aybekler.api.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import aybeklerApp.aybekler.business.abstracts.AllAccountSignalsService;
+import aybeklerApp.aybekler.core.utilities.results.DataResult;
 import aybeklerApp.aybekler.entities.concretes.AllAccountSignals;
+import aybeklerApp.aybekler.entities.concretes.Muco;
 
 @RestController
 @RequestMapping("/api/allaccountsignals")
 public class AllAccountSignalsController {
 	
+	private AllAccountSignalsService allAccountSignalsService;
+	
 	@Autowired
-	public AllAccountSignalsController() {
+	public AllAccountSignalsController(AllAccountSignalsService allAccountSignalsService) {
 		super();
-		
+		this.allAccountSignalsService = allAccountSignalsService;
 	}
 	
 	@GetMapping("/getall")
-	public void getAll() {
-		System.out.println("It's working");
+	public DataResult<List<AllAccountSignals>> getAll() {
+		return this.allAccountSignalsService.getAll();
 	}
 }
