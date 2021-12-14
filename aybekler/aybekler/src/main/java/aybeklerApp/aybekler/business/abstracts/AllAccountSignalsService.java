d910287776278929c580364d1ba1718d5dd3c9d4
ByrMucahit <m.mucahitbayar@gmail.com>
2021-12-14 19:47:34 +0300

    Service
    Some transaction has been added to service.

--- aybekler/aybekler/src/main/java/aybeklerApp/aybekler/business/abstracts/AllAccountSignalsService.java
+++ aybekler/aybekler/src/main/java/aybeklerApp/aybekler/business/abstracts/AllAccountSignalsService.java
@@ -2,9 +2,17 @@ package aybeklerApp.aybekler.business.abstracts;
 import aybeklerApp.aybekler.entities.concretes.AllAccountSignals;
 import aybeklerApp.aybekler.entities.concretes.Muco;
 import aybeklerApp.aybekler.core.utilities.results.DataResult;
+import aybeklerApp.aybekler.core.utilities.results.Result;
 
 import java.util.List;
 
 public interface AllAccountSignalsService {
 	DataResult<List<AllAccountSignals>> getAll();
-}
+	Result insertInto(AllAccountSignals allAccountSignals);
+	DataResult<List<AllAccountSignals>> printAsExcell();
+	DataResult<List<AllAccountSignals>> getDataByRegion();
+	DataResult<List<AllAccountSignals>> getDataByOccupation();
+	DataResult<List<AllAccountSignals>> getDataByPeriod();
+	DataResult<List<AllAccountSignals>> getDataByDate();
+	DataResult<List<AllAccountSignals>> getDataByClock();
+	}
