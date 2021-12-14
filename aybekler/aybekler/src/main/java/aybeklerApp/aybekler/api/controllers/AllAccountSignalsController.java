bbbbdcf4724c41c7c54940be86f90ba366525c55
ByrMucahit <m.mucahitbayar@gmail.com>
2021-12-14 19:46:43 +0300

    API
    Controller for all account signal from API dimeinsion has been buit.

--- aybekler/aybekler/src/main/java/aybeklerApp/aybekler/api/controllers/AllAccountSignalsController.java
+++ aybekler/aybekler/src/main/java/aybeklerApp/aybekler/api/controllers/AllAccountSignalsController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import aybeklerApp.aybekler.business.abstracts.AllAccountSignalsService;
 import aybeklerApp.aybekler.core.utilities.results.DataResult;
+import aybeklerApp.aybekler.core.utilities.results.Result;
 import aybeklerApp.aybekler.entities.concretes.AllAccountSignals;
 import aybeklerApp.aybekler.entities.concretes.Muco;
 
@@ -27,5 +30,10 @@ public class AllAccountSignalsController {
 	@GetMapping("/getall")
 	public DataResult<List<AllAccountSignals>> getAll() {
 		return this.allAccountSignalsService.getAll();
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody AllAccountSignals allAccountSignals) {
+		return this.allAccountSignalsService.insertInto(allAccountSignals);
 	}
 }
