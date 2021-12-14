575a0c7f1a26363f64cd34b4eece7c5ac682881b
ByrMucahit <m.mucahitbayar@gmail.com>
2021-12-14 19:47:56 +0300

    Manager
    Some transaction has been applied in business dimension.

--- aybekler/aybekler/src/main/java/aybeklerApp/aybekler/business/concretes/AllAccountSignalsManager.java
+++ aybekler/aybekler/src/main/java/aybeklerApp/aybekler/business/concretes/AllAccountSignalsManager.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import aybeklerApp.aybekler.business.abstracts.AllAccountSignalsService;
 import aybeklerApp.aybekler.core.utilities.results.DataResult;
+import aybeklerApp.aybekler.core.utilities.results.Result;
 import aybeklerApp.aybekler.core.utilities.results.SuccessDataResult;
+import aybeklerApp.aybekler.core.utilities.results.SuccessResult;
 import aybeklerApp.aybekler.dataAccess.abstracts.AllAccountSignalsDao;
 import aybeklerApp.aybekler.entities.concretes.AllAccountSignals;
 import aybeklerApp.aybekler.entities.concretes.Muco;
@@ -25,10 +27,50 @@ public class AllAccountSignalsManager implements AllAccountSignalsService {
 
 	@Override
 	public DataResult<List<AllAccountSignals>> getAll() {
-		// TODO Auto-generated method stub
-		System.out.println("parameters"+this.allAccountSignalsDao.findAll());
 		return new SuccessDataResult<List<AllAccountSignals>>
 			(this.allAccountSignalsDao.findAll(), "Data has been listed");
+	}
+
+	@Override
+	public Result insertInto(AllAccountSignals allAccountSignals) {
+		this.allAccountSignalsDao.save(allAccountSignals);
+		return new SuccessResult("Product has been listed");
+	}
+
+	@Override
+	public DataResult<List<AllAccountSignals>> printAsExcell() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<List<AllAccountSignals>> getDataByRegion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<List<AllAccountSignals>> getDataByOccupation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<List<AllAccountSignals>> getDataByPeriod() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<List<AllAccountSignals>> getDataByDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<List<AllAccountSignals>> getDataByClock() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 
