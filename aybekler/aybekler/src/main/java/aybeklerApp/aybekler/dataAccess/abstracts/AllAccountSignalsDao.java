573832eca63481c503ff9aa58fa313f08a30f14e
ByrMucahit <m.mucahitbayar@gmail.com>
2021-12-14 19:46:07 +0300

    Data Access for All Account
    Data Access dimension for All Account has been built

--- aybekler/aybekler/src/main/java/aybeklerApp/aybekler/dataAccess/abstracts/AllAccountSignalsDao.java
+++ aybekler/aybekler/src/main/java/aybeklerApp/aybekler/dataAccess/abstracts/AllAccountSignalsDao.java
@@ -3,9 +3,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import aybeklerApp.aybekler.core.utilities.results.DataResult;
 import aybeklerApp.aybekler.entities.concretes.AllAccountSignals;
 
 public interface AllAccountSignalsDao extends JpaRepository<AllAccountSignals, Integer> {
 	
-	
+	List<AllAccountSignals> getDataOnUserDesired(String region, String occupation, String perion, String date, String hour);
 }
